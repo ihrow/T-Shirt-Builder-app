c5dd25e00050222c9e388d3f264550c4bfa6823d
igor <igornotfound@gmail.com>
2023-04-13 17:16:25 +0200

    custom button

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,23 @@
+import { useSnapshot } from "valtio";
+import { state } from "../store";
+
+export function CustomButton({ type, title, customStyles, handleClick }) {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "white",
+      };
+    }
+  };
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+}
